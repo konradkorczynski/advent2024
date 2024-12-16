1d546fee0fb12f14d06556b563030767ac70d98a
Konrad Korczynski <konrad.korczynski@gmail.com>
2024-12-16 19:37:14 +0000

    day16t1 wip

---
+++ src/day16/task1/index.ts
@@ -0,0 +1,247 @@
+import * as fs from "fs";
+import { withMetrics } from "../../utils";
+import util from "util";
+
+// const mapInput = "input.txt";
+const mapInput = "example_input.txt";
+
+type Direction = "N" | "E" | "S" | "W";
+type CellValue = "#" | ".";
+
+class Cell {
+  constructor(
+    public y: number,
+    public x: number,
+    public value: CellValue,
+    public directions: Direction[],
+    public visited = false
+  ) {}
+}
+
+const startPosition = { y: 0, x: 0 };
+const endPosition = { y: 0, x: 0 };
+const paths: Path[] = [];
+
+class Path {
+  constructor(
+    public currentCell: Cell,
+    public cost: number,
+    public moveDirection: Direction,
+    public visitedJunctions: string[] = []
+  ) {}
+}
+
+const getLine = (map: Map, cell: Cell, direction: Direction) => {
+  const line = [];
+  if (direction === "E") {
+    for (let i = cell.x; i < map[cell.y].length; i++) {
+      line.push(map[cell.y][i]);
+    }
+  } else if (direction === "W") {
+    for (let i = cell.x; i >= 0; i--) {
+      line.push(map[cell.y][i]);
+    }
+  } else if (direction === "N") {
+    for (let i = cell.y; i >= 0; i--) {
+      line.push(map[i][cell.x]);
+    }
+  } else if (direction === "S") {
+    for (let i = cell.y; i < map.length; i++) {
+      line.push(map[i][cell.x]);
+    }
+  }
+
+  const wallIndex = line.findIndex((cell) => cell.value === "#");
+  const firstJunction = line
+    .slice(1, line.length)
+    .findIndex(
+      (cell) =>
+        cell.directions.length > 2 ||
+        (!cell.directions.includes(direction) && cell.directions.length === 2)
+    );
+  console.log({ wallIndex, firstJunction });
+  if (firstJunction === -1 || firstJunction + 1 > wallIndex)
+    return line.slice(0, wallIndex + 1);
+
+  return line.slice(0, firstJunction + 2);
+};
+
+type MoveResult = "deadend" | "junction" | "success" | "continue";
+
+const move = (map: Map, path: Path): MoveResult => {
+  // move to next available cell - junction or dead end
+  const line = getLine(map, path.currentCell, path.moveDirection);
+  // console.log(util.inspect({ line }, false, null, true));
+  path.cost = path.cost + line.length - 1;
+  path.currentCell = line[line.length - 1];
+
+  const availableDirections = path.currentCell.directions.filter(
+    (direction) => direction !== getOppositeDirection(path.moveDirection)
+  );
+
+  console.log({ availableDirections });
+
+  if (availableDirections.length === 0) {
+    if (
+      path.currentCell.x === endPosition.x &&
+      path.currentCell.y === endPosition.y
+    ) {
+      console.log("found FINISH");
+      return "success";
+    }
+    return "deadend";
+  }
+
+  // bend in line
+  if (availableDirections.length === 1) {
+    path.cost = path.cost + 1000;
+    path.moveDirection = availableDirections[0];
+    return "continue";
+  }
+
+  if (
+    path.visitedJunctions.includes(
+      path.currentCell.y + "-" + path.currentCell.x
+    )
+  ) {
+    return "deadend";
+  }
+  path.visitedJunctions.push(path.currentCell.y + "-" + path.currentCell.x);
+
+  availableDirections.forEach((direction) => {
+    const cost = path.moveDirection === direction ? 0 : 1000;
+    console.log(`adding path`, {
+      direction,
+      cost,
+      visited: path.visitedJunctions,
+    });
+    paths.push(
+      new Path(
+        path.currentCell,
+        path.cost + cost,
+        direction,
+        path.visitedJunctions
+      )
+    );
+  });
+  return "junction";
+};
+
+type Map = Cell[][];
+
+const drawMap = (map: Map) => {
+  map.forEach((row) => {
+    console.log(row.map((cell) => cell.value).join(""));
+  });
+};
+
+const inLineMoves = ["NS", "SN", "EW", "WE"];
+const isInLineMove = (direction1: Direction, direction2: Direction) => {
+  return inLineMoves.includes(direction1 + direction2);
+};
+
+const getOppositeDirection = (direction: Direction): Direction => {
+  if (direction === "N") return "S";
+  if (direction === "S") return "N";
+  if (direction === "E") return "W";
+  return "E";
+};
+
+const run = () => {
+  console.log("------------ start ------------");
+
+  const map: Map = fs
+    .readFileSync(mapInput, "utf8")
+    .split("\n")
+    .map((row, y) => {
+      return row.split("").map((cell, x) => {
+        if (cell === "S") {
+          startPosition.y = y;
+          startPosition.x = x;
+        }
+        if (cell === "E") {
+          endPosition.y = y;
+          endPosition.x = x;
+        }
+        return new Cell(
+          y,
+          x,
+          cell === "S" || cell === "E" ? "." : (cell as CellValue),
+          []
+        );
+      });
+    });
+  // no idea if this will work, but the assuption is that all paths are done
+  // when all cells are visited
+  let cellstToVisit = 0;
+
+  let score = 0;
+  // put directions on cells
+  map
+    .flat(1)
+    .filter((cell) => cell.value === ".")
+    .forEach((cell) => {
+      const directions: Direction[] = [];
+      if (map[cell.y - 1][cell.x]?.value === ".") {
+        directions.push("N");
+      }
+      if (map[cell.y + 1][cell.x]?.value === ".") {
+        directions.push("S");
+      }
+      if (map[cell.y][cell.x - 1]?.value === ".") {
+        directions.push("W");
+      }
+      if (map[cell.y][cell.x + 1]?.value === ".") {
+        directions.push("E");
+      }
+      cell.directions = directions;
+      cellstToVisit++;
+    });
+
+  // console.log({ cellstToVisit, startPosition, endPosition });
+  let position = { ...startPosition };
+  const currentCell = map[position.y][position.x];
+
+  let debugIteration = 0;
+  map[startPosition.y][startPosition.x].directions.forEach((direction) => {
+    const cost = isInLineMove("W", direction) ? 0 : 1000;
+    paths.push(
+      new Path(currentCell, cost, direction, [
+        currentCell.y + "-" + currentCell.x,
+      ])
+    );
+  });
+
+  while (debugIteration < 5) {
+    const path = paths[0];
+    console.log(`\n--------> making move`, {
+      from: path.currentCell.y + "-" + path.currentCell.x,
+      direction: path.moveDirection,
+    });
+    const moveResult = move(map, path);
+    console.log(util.inspect({ path }, false, null, true));
+    if (
+      moveResult === "deadend" ||
+      moveResult === "success" ||
+      moveResult === "junction"
+    ) {
+      console.log(`XXXXXXXXXXXXX removing path`);
+      paths.shift();
+    }
+    if (moveResult === "success") {
+      console.log(path.cost);
+    }
+    console.log(
+      util.inspect({ moveResult, pathsLength: paths.length }, false, null, true)
+    );
+
+    debugIteration++;
+  }
+
+  drawMap(map);
+  // console.log({ moves });
+
+  return 0;
+};
+
+withMetrics(run);
---
+++ src/day16/task2/index.ts
@@ -0,0 +1,296 @@
+import * as fs from "fs";
+import { withMetrics } from "../../utils";
+import util from "util";
+
+const mapInput = "input.txt";
+const movesInput = "moves.txt";
+// const mapInput = "example_input.txt";
+// const movesInput = "example_moves.txt";
+
+type CellValue = "#" | "." | "[" | "]" | "@";
+
+class Cell {
+  constructor(public y: number, public x: number, public value: CellValue) {}
+}
+
+type Map = Cell[][];
+type Move = ">" | "<" | "^" | "v";
+
+const getLine = (map: Map, y: number, x: number, move: Move) => {
+  const line = [];
+  if (move === ">") {
+    for (let i = x; i < map[y].length; i++) {
+      line.push(map[y][i]);
+    }
+  } else if (move === "<") {
+    for (let i = x; i >= 0; i--) {
+      line.push(map[y][i]);
+    }
+  } else if (move === "^") {
+    for (let i = y; i >= 0; i--) {
+      line.push(map[i][x]);
+    }
+  } else if (move === "v") {
+    for (let i = y; i < map.length; i++) {
+      line.push(map[i][x]);
+    }
+  }
+  return line;
+};
+
+let movedBoxes: string[] = [];
+
+class Box {
+  public isMovable: boolean = false;
+  private boxesAlongTheWay: Box[] = [];
+  private moveDirectionValue: number;
+  private move: Move;
+  private map: Map;
+  private leftCell: Cell;
+  private rightCell: Cell;
+  public hasMoved: boolean = false;
+  constructor(leftCell: Cell, rightCell: Cell, map: Map, move: Move) {
+    this.move = move;
+    this.map = map;
+    this.leftCell = leftCell;
+    this.rightCell = rightCell;
+
+    this.moveDirectionValue = move === "^" ? leftCell.y - 1 : leftCell.y + 1;
+    this.boxesAlongTheWay = this.getNeighboringBoxes();
+    this.isMovable = this.checkIfMovable();
+  }
+
+  private getNeighboringBoxes(): Box[] {
+    const boxes = [];
+    // one box above
+    if (
+      this.map[this.moveDirectionValue][this.leftCell.x].value === "[" &&
+      this.map[this.moveDirectionValue][this.rightCell.x].value === "]"
+    ) {
+      boxes.push(
+        new Box(
+          this.map[this.moveDirectionValue][this.leftCell.x],
+          this.map[this.moveDirectionValue][this.rightCell.x],
+          this.map,
+          this.move
+        )
+      );
+    }
+    // one box above left
+    if (this.map[this.moveDirectionValue][this.leftCell.x].value === "]") {
+      boxes.push(
+        new Box(
+          this.map[this.moveDirectionValue][this.leftCell.x - 1],
+          this.map[this.moveDirectionValue][this.leftCell.x],
+          this.map,
+          this.move
+        )
+      );
+    }
+    // one box above right
+    if (this.map[this.moveDirectionValue][this.rightCell.x].value === "[") {
+      boxes.push(
+        new Box(
+          this.map[this.moveDirectionValue][this.rightCell.x],
+          this.map[this.moveDirectionValue][this.rightCell.x + 1],
+          this.map,
+          this.move
+        )
+      );
+    }
+    return boxes;
+  }
+
+  private checkIfMovable(): boolean {
+    // blocked by wall
+    if (
+      this.map[this.moveDirectionValue][this.leftCell.x].value === "#" ||
+      this.map[this.moveDirectionValue][this.rightCell.x].value === "#"
+    ) {
+      return false;
+    }
+    // empty space
+    if (
+      this.map[this.moveDirectionValue][this.leftCell.x].value === "." &&
+      this.map[this.moveDirectionValue][this.rightCell.x].value === "."
+    ) {
+      return true;
+    }
+    // box along the way
+    return this.boxesAlongTheWay.every((box) => box.isMovable);
+  }
+
+  moveBox() {
+    if (this.isMovable) {
+      this.boxesAlongTheWay.forEach((box) => {
+        box.moveBox();
+      });
+      // drawMap(this.map);
+      const movedBoxeKey = `${this.leftCell.y},${this.leftCell.x}`;
+      if (movedBoxes.includes(movedBoxeKey)) {
+        return false;
+      }
+      movedBoxes.push(movedBoxeKey);
+      this.leftCell.value = ".";
+      this.rightCell.value = ".";
+      this.map[this.moveDirectionValue][this.leftCell.x].value = "[";
+      this.map[this.moveDirectionValue][this.rightCell.x].value = "]";
+    }
+    return this.isMovable;
+  }
+
+  // clean up if this makes any sense - might later, dunno
+  clear() {
+    this.boxesAlongTheWay.forEach((box) => {
+      box.clear();
+    });
+    this.boxesAlongTheWay = [];
+  }
+}
+
+const moveRobot = (
+  map: Map,
+  robotPosition: { y: number; x: number },
+  move: Move
+) => {
+  const { y, x } = robotPosition;
+  const line = getLine(map, y, x, move);
+  // console.log("making move", { line, move });
+  if (move === ">" || move === "<") {
+    const wallIndex = line.findIndex((cell) => cell.value === "#");
+    const firstEmptyCellIndex = line.findIndex((cell) => cell.value === ".");
+    // console.log({ wallIndex, firstEmptyCellIndex });
+    if (firstEmptyCellIndex === -1 || wallIndex < firstEmptyCellIndex) return;
+
+    const subLine = line.slice(0, firstEmptyCellIndex + 1);
+    const arr = subLine.map((cell) => cell.value);
+    arr.unshift(...arr.splice(-1));
+    subLine.forEach((cell, index) => {
+      const newValue = arr[index];
+      if (newValue === "@") {
+        robotPosition.y = cell.y;
+        robotPosition.x = cell.x;
+      }
+      cell.value = arr[index];
+    });
+  } else {
+    //  don't move - wall
+    if (line[1].value === "#") return;
+    // move straight to the empty space
+    if (line[1].value === ".") {
+      robotPosition.y = line[1].y;
+      robotPosition.x = line[1].x;
+      line[0].value = ".";
+      line[1].value = "@";
+      return;
+    }
+    // create a box and move if you can
+    const moveDirectionValue =
+      move === "^" ? robotPosition.y - 1 : robotPosition.y + 1;
+    let box: Box | null = null;
+    if (line[1].value === "[") {
+      box = new Box(
+        map[moveDirectionValue][robotPosition.x],
+        map[moveDirectionValue][robotPosition.x + 1],
+        map,
+        move
+      );
+    } else if (line[1].value === "]") {
+      box = new Box(
+        map[moveDirectionValue][robotPosition.x - 1],
+        map[moveDirectionValue][robotPosition.x],
+        map,
+        move
+      );
+    }
+
+    if (box?.moveBox()) {
+      robotPosition.y = line[1].y;
+      robotPosition.x = line[1].x;
+      line[0].value = ".";
+      line[1].value = "@";
+    }
+    box?.clear();
+  }
+  // console.log({ arr, line });
+};
+
+const drawMap = (map: Map, toFile?: boolean, move?: Move) => {
+  if (toFile && move) {
+    fs.appendFileSync("output.txt", "\n");
+  }
+  map.forEach((row) => {
+    if (toFile) {
+      fs.appendFileSync(
+        "output.txt",
+        row
+          .map((cell) => (cell.value === "@" && move ? move : cell.value))
+          .join("")
+      );
+      fs.appendFileSync("output.txt", "\n");
+    } else {
+      console.log(
+        row
+          .map((cell) => (cell.value === "@" && move ? move : cell.value))
+          .join("")
+      );
+    }
+  });
+};
+
+const run = () => {
+  // console.log("------------ start ------------");
+  const robotPosition = { y: 0, x: 0 };
+
+  const map: Map = fs
+    .readFileSync(mapInput, "utf8")
+    .split("\n")
+    .map((row, y) => {
+      return row.split("").flatMap((cell, x) => {
+        if (cell === "@") {
+          robotPosition.y = y;
+          robotPosition.x = x * 2;
+          return [
+            new Cell(y, x * 2, cell as CellValue),
+            new Cell(y, x * 2 + 1, "." as CellValue),
+          ];
+        }
+        if (cell === "O") {
+          return [
+            new Cell(y, x * 2, "[" as CellValue),
+            new Cell(y, x * 2 + 1, "]" as CellValue),
+          ];
+        }
+        return [
+          new Cell(y, x * 2, cell as CellValue),
+          new Cell(y, x * 2 + 1, cell as CellValue),
+        ];
+      });
+    });
+
+  const moves = fs.readFileSync(movesInput, "utf8").split("\n").join("");
+
+  const toFile = false;
+
+  drawMap(map, toFile);
+  moves.split("").forEach((move, i) => {
+    moveRobot(map, robotPosition, move as Move);
+    movedBoxes = [];
+    sleep(10);
+    console.clear();
+    drawMap(map, toFile, move as Move);
+    console.log("move: ", i);
+  });
+  // console.log({ robotPosition, moves });
+
+  return map
+    .flat()
+    .filter((cell) => cell.value === "[")
+    .reduce((acc, curr) => acc + curr.x + 100 * curr.y, 0);
+};
+
+withMetrics(run);
+
+function sleep(n: number) {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
