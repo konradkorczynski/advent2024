63130998813c3c7b641b6579bfb1949b1c1aa3c2
Konrad Korczynski <konrad.korczynski@beamery.com>
2024-12-17 00:02:23 +0000

    stuck on day16t2

--- src/day16/task2/index.ts
+++ src/day16/task2/index.ts
@@ -2,295 +2,205 @@ import * as fs from "fs";
 import { withMetrics } from "../../utils";
 import util from "util";
 
-const mapInput = "input.txt";
-const movesInput = "moves.txt";
-// const mapInput = "example_input.txt";
-// const movesInput = "example_moves.txt";
+// const mapInput = "input.txt";
+const mapInput = "example_input.txt";
 
-type CellValue = "#" | "." | "[" | "]" | "@";
+type Direction = "N" | "E" | "S" | "W";
+type CellValue = "#" | "." | "S" | "E";
 
 class Cell {
-  constructor(public y: number, public x: number, public value: CellValue) {}
+  constructor(
+    public y: number,
+    public x: number,
+    public value: CellValue,
+    public directions: Direction[]
+  ) {}
 }
-
+const startPosition = { y: 0, x: 0 };
+const endPosition = { y: 0, x: 0 };
 type Map = Cell[][];
-type Move = ">" | "<" | "^" | "v";
+type Graph = Record<string, Record<string, number>>;
 
-const getLine = (map: Map, y: number, x: number, move: Move) => {
+const graph: Graph = {};
+
+const getLine = (map: Map, cell: Cell, direction: Direction) => {
   const line = [];
-  if (move === ">") {
-    for (let i = x; i < map[y].length; i++) {
-      line.push(map[y][i]);
+  if (direction === "E") {
+    for (let i = cell.x; i < map[cell.y].length; i++) {
+      line.push(map[cell.y][i]);
     }
-  } else if (move === "<") {
-    for (let i = x; i >= 0; i--) {
-      line.push(map[y][i]);
+  } else if (direction === "W") {
+    for (let i = cell.x; i >= 0; i--) {
+      line.push(map[cell.y][i]);
     }
-  } else if (move === "^") {
-    for (let i = y; i >= 0; i--) {
-      line.push(map[i][x]);
+  } else if (direction === "N") {
+    for (let i = cell.y; i >= 0; i--) {
+      line.push(map[i][cell.x]);
     }
-  } else if (move === "v") {
-    for (let i = y; i < map.length; i++) {
-      line.push(map[i][x]);
+  } else if (direction === "S") {
+    for (let i = cell.y; i < map.length; i++) {
+      line.push(map[i][cell.x]);
     }
   }
-  return line;
+
+  const wallIndex = line.findIndex((cell) => cell.value === "#");
+  const firstJunction = line
+    .slice(1, line.length)
+    .findIndex(
+      (cell) =>
+        cell.directions.length > 2 ||
+        (!cell.directions.includes(direction) && cell.directions.length === 2)
+    );
+  // console.log({ wallIndex, firstJunction });
+  if (firstJunction === -1 || firstJunction + 1 > wallIndex)
+    return line.slice(0, wallIndex);
+
+  return line.slice(0, firstJunction + 2);
 };
 
-let movedBoxes: string[] = [];
+const allDirections: Direction[] = ["N", "E", "S", "W"];
 
-class Box {
-  public isMovable: boolean = false;
-  private boxesAlongTheWay: Box[] = [];
-  private moveDirectionValue: number;
-  private move: Move;
-  private map: Map;
-  private leftCell: Cell;
-  private rightCell: Cell;
-  public hasMoved: boolean = false;
-  constructor(leftCell: Cell, rightCell: Cell, map: Map, move: Move) {
-    this.move = move;
-    this.map = map;
-    this.leftCell = leftCell;
-    this.rightCell = rightCell;
-
-    this.moveDirectionValue = move === "^" ? leftCell.y - 1 : leftCell.y + 1;
-    this.boxesAlongTheWay = this.getNeighboringBoxes();
-    this.isMovable = this.checkIfMovable();
-  }
-
-  private getNeighboringBoxes(): Box[] {
-    const boxes = [];
-    // one box above
-    if (
-      this.map[this.moveDirectionValue][this.leftCell.x].value === "[" &&
-      this.map[this.moveDirectionValue][this.rightCell.x].value === "]"
-    ) {
-      boxes.push(
-        new Box(
-          this.map[this.moveDirectionValue][this.leftCell.x],
-          this.map[this.moveDirectionValue][this.rightCell.x],
-          this.map,
-          this.move
-        )
-      );
-    }
-    // one box above left
-    if (this.map[this.moveDirectionValue][this.leftCell.x].value === "]") {
-      boxes.push(
-        new Box(
-          this.map[this.moveDirectionValue][this.leftCell.x - 1],
-          this.map[this.moveDirectionValue][this.leftCell.x],
-          this.map,
-          this.move
-        )
-      );
-    }
-    // one box above right
-    if (this.map[this.moveDirectionValue][this.rightCell.x].value === "[") {
-      boxes.push(
-        new Box(
-          this.map[this.moveDirectionValue][this.rightCell.x],
-          this.map[this.moveDirectionValue][this.rightCell.x + 1],
-          this.map,
-          this.move
-        )
-      );
-    }
-    return boxes;
-  }
-
-  private checkIfMovable(): boolean {
-    // blocked by wall
-    if (
-      this.map[this.moveDirectionValue][this.leftCell.x].value === "#" ||
-      this.map[this.moveDirectionValue][this.rightCell.x].value === "#"
-    ) {
-      return false;
-    }
-    // empty space
-    if (
-      this.map[this.moveDirectionValue][this.leftCell.x].value === "." &&
-      this.map[this.moveDirectionValue][this.rightCell.x].value === "."
-    ) {
-      return true;
-    }
-    // box along the way
-    return this.boxesAlongTheWay.every((box) => box.isMovable);
-  }
-
-  moveBox() {
-    if (this.isMovable) {
-      this.boxesAlongTheWay.forEach((box) => {
-        box.moveBox();
-      });
-      // drawMap(this.map);
-      const movedBoxeKey = `${this.leftCell.y},${this.leftCell.x}`;
-      if (movedBoxes.includes(movedBoxeKey)) {
-        return false;
+const addLastToGraph = (map: Map, cell: Cell, direction: Direction) => {
+  allDirections.forEach((dir) => {
+    const line = getLine(map, cell, direction);
+    const last = line.pop();
+    const cellKey = `${cell.y},${cell.x},${dir}`;
+    if (last && isNode(last)) {
+      // console.log({ direction, last, line });
+      if (!graph[cellKey]) {
+        graph[cellKey] = {};
       }
-      movedBoxes.push(movedBoxeKey);
-      this.leftCell.value = ".";
-      this.rightCell.value = ".";
-      this.map[this.moveDirectionValue][this.leftCell.x].value = "[";
-      this.map[this.moveDirectionValue][this.rightCell.x].value = "]";
-    }
-    return this.isMovable;
-  }
-
-  // clean up if this makes any sense - might later, dunno
-  clear() {
-    this.boxesAlongTheWay.forEach((box) => {
-      box.clear();
-    });
-    this.boxesAlongTheWay = [];
-  }
-}
-
-const moveRobot = (
-  map: Map,
-  robotPosition: { y: number; x: number },
-  move: Move
-) => {
-  const { y, x } = robotPosition;
-  const line = getLine(map, y, x, move);
-  // console.log("making move", { line, move });
-  if (move === ">" || move === "<") {
-    const wallIndex = line.findIndex((cell) => cell.value === "#");
-    const firstEmptyCellIndex = line.findIndex((cell) => cell.value === ".");
-    // console.log({ wallIndex, firstEmptyCellIndex });
-    if (firstEmptyCellIndex === -1 || wallIndex < firstEmptyCellIndex) return;
-
-    const subLine = line.slice(0, firstEmptyCellIndex + 1);
-    const arr = subLine.map((cell) => cell.value);
-    arr.unshift(...arr.splice(-1));
-    subLine.forEach((cell, index) => {
-      const newValue = arr[index];
-      if (newValue === "@") {
-        robotPosition.y = cell.y;
-        robotPosition.x = cell.x;
-      }
-      cell.value = arr[index];
-    });
-  } else {
-    //  don't move - wall
-    if (line[1].value === "#") return;
-    // move straight to the empty space
-    if (line[1].value === ".") {
-      robotPosition.y = line[1].y;
-      robotPosition.x = line[1].x;
-      line[0].value = ".";
-      line[1].value = "@";
-      return;
-    }
-    // create a box and move if you can
-    const moveDirectionValue =
-      move === "^" ? robotPosition.y - 1 : robotPosition.y + 1;
-    let box: Box | null = null;
-    if (line[1].value === "[") {
-      box = new Box(
-        map[moveDirectionValue][robotPosition.x],
-        map[moveDirectionValue][robotPosition.x + 1],
-        map,
-        move
-      );
-    } else if (line[1].value === "]") {
-      box = new Box(
-        map[moveDirectionValue][robotPosition.x - 1],
-        map[moveDirectionValue][robotPosition.x],
-        map,
-        move
-      );
-    }
-
-    if (box?.moveBox()) {
-      robotPosition.y = line[1].y;
-      robotPosition.x = line[1].x;
-      line[0].value = ".";
-      line[1].value = "@";
-    }
-    box?.clear();
-  }
-  // console.log({ arr, line });
-};
-
-const drawMap = (map: Map, toFile?: boolean, move?: Move) => {
-  if (toFile && move) {
-    fs.appendFileSync("output.txt", "\n");
-  }
-  map.forEach((row) => {
-    if (toFile) {
-      fs.appendFileSync(
-        "output.txt",
-        row
-          .map((cell) => (cell.value === "@" && move ? move : cell.value))
-          .join("")
-      );
-      fs.appendFileSync("output.txt", "\n");
-    } else {
-      console.log(
-        row
-          .map((cell) => (cell.value === "@" && move ? move : cell.value))
-          .join("")
-      );
+      const lastKey = `${last.y},${last.x},${direction}`;
+      graph[cellKey][lastKey] = line.length + (dir === direction ? 0 : 1000);
     }
   });
 };
 
+const isNode = (cell: Cell) =>
+  cell.directions.length > 2 ||
+  cell.value === "S" ||
+  cell.value === "E" ||
+  (cell.directions.length === 2 &&
+    !inLineDirections.includes(cell.directions.join("")));
+
+const inLineDirections = ["NS", "SN", "EW", "WE"];
+
+const dijkstra = (graph: Graph, start: string) => {
+  // Create an object to store the shortest distance from the start node to every other node
+  type DistanceData = { d: number; s: string[] };
+  let distances: Record<string, DistanceData[]> = {};
+
+  // Get all the nodes of the graph
+  let nodes = Object.keys(graph);
+
+  // Initially, set the shortest distance to every node as Infinity
+  for (let node of nodes) {
+    distances[node] = [{ d: Infinity, s: [] }];
+  }
+
+  // The distance from the start node to itself is 0
+  distances[start] = [{ d: 0, s: [start] }];
+
+  // Loop until all nodes are visited
+  while (nodes.length) {
+    // Sort nodes by distance and pick the closest unvisited node
+    nodes.sort((a, b) => distances[a][0].d - distances[b][0].d);
+
+    let closestNode = nodes.shift();
+
+    const dst = distances[closestNode!];
+    dst.forEach((distanceData) => {
+      // If the shortest distance to the closest node is still Infinity, then remaining nodes are unreachable and we can break
+      if (distanceData.d === Infinity) return;
+
+      // For each neighboring node of the current node
+      for (let neighbor in graph[closestNode!]) {
+        // Calculate tentative distance to the neighboring node
+        let newDistance = distanceData.d + graph[closestNode!][neighbor];
+
+        // If the newly calculated distance is shorter than the previously known distance to this neighbor
+        if (newDistance < distanceData.d) {
+          // Update the shortest distance to this neighbor
+          distances[neighbor].push({
+            d: newDistance,
+            s: [...distanceData.s, neighbor],
+          });
+        }
+      }
+    });
+  }
+
+  // Return the shortest distance from the start node to all nodes
+  return distances;
+};
+
 const run = () => {
   // console.log("------------ start ------------");
-  const robotPosition = { y: 0, x: 0 };
 
   const map: Map = fs
     .readFileSync(mapInput, "utf8")
     .split("\n")
     .map((row, y) => {
-      return row.split("").flatMap((cell, x) => {
-        if (cell === "@") {
-          robotPosition.y = y;
-          robotPosition.x = x * 2;
-          return [
-            new Cell(y, x * 2, cell as CellValue),
-            new Cell(y, x * 2 + 1, "." as CellValue),
-          ];
+      return row.split("").map((cell, x) => {
+        if (cell === "S") {
+          startPosition.y = y;
+          startPosition.x = x;
         }
-        if (cell === "O") {
-          return [
-            new Cell(y, x * 2, "[" as CellValue),
-            new Cell(y, x * 2 + 1, "]" as CellValue),
-          ];
+        if (cell === "E") {
+          endPosition.y = y;
+          endPosition.x = x;
         }
-        return [
-          new Cell(y, x * 2, cell as CellValue),
-          new Cell(y, x * 2 + 1, cell as CellValue),
-        ];
+        return new Cell(y, x, cell as CellValue, []);
       });
     });
 
-  const moves = fs.readFileSync(movesInput, "utf8").split("\n").join("");
+  // put directions on cells
+  map
+    .flat(1)
+    .filter(
+      (cell) => cell.value === "." || cell.value === "S" || cell.value === "E"
+    )
+    .forEach((cell) => {
+      const directions: Direction[] = [];
+      if (map[cell.y - 1][cell.x]?.value === ".") {
+        directions.push("N");
+      }
+      if (map[cell.y + 1][cell.x]?.value === ".") {
+        directions.push("S");
+      }
+      if (map[cell.y][cell.x - 1]?.value === ".") {
+        directions.push("W");
+      }
+      if (map[cell.y][cell.x + 1]?.value === ".") {
+        directions.push("E");
+      }
+      cell.directions = directions;
+    });
 
-  const toFile = false;
+  // sort out graph
+  map
+    .flat(1)
+    .filter((cell) => {
+      const notWall = cell.value === ".";
+      return (
+        (notWall && isNode(cell)) || cell.value === "S" || cell.value === "E"
+      );
+    })
+    .forEach((cell) => {
+      //   console.log({ cell });
+      cell.directions.forEach((direction) => {
+        addLastToGraph(map, cell, direction);
+      });
+    });
 
-  drawMap(map, toFile);
-  moves.split("").forEach((move, i) => {
-    moveRobot(map, robotPosition, move as Move);
-    movedBoxes = [];
-    sleep(10);
-    console.clear();
-    drawMap(map, toFile, move as Move);
-    console.log("move: ", i);
-  });
-  // console.log({ robotPosition, moves });
+  //   console.log(util.inspect(graph, false, null, true /* enable colors */));
+  const distances = dijkstra(graph, `${startPosition.y},${startPosition.x},E`);
+  console.log(
+    util.inspect({ distances }, false, null, true /* enable colors */)
+  );
+  console.log(distances[`${endPosition.y},${endPosition.x},N`]);
 
-  return map
-    .flat()
-    .filter((cell) => cell.value === "[")
-    .reduce((acc, curr) => acc + curr.x + 100 * curr.y, 0);
+  return 0;
 };
 
 withMetrics(run);
-
-function sleep(n: number) {
-  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
-}
